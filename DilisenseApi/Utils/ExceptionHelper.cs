e3119c5f5ab98986e2b35e9b665fe55e30960b36
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2025-07-04 09:34:36 +0200

    enabled nullable reference types, added utils helpers

---
+++ DilisenseApi/Utils/ExceptionHelper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DilisenseApi.Utils {
+    internal static class ExceptionHelper {
+        public const string ExceptionDataKey = "Content";
+
+        public static T WithContent<T>(this T ex, string? content) where T : Exception {
+            ex.Data[ExceptionDataKey] = content;
+            return ex;
+        }
+    }
+}
---
+++ DilisenseApi/Utils/RestResponseHandler.cs
@@ -0,0 +1,13 @@
+using RestSharp;
+
+namespace DilisenseApi.Utils {
+    internal static class RestResponseHandler {
+        public static T Handle<T>(RestResponse<T> response) {
+            if (response.ErrorException != null)
+                throw response.ErrorException.WithContent(response.Content);
+            if (response.Data == null)
+                throw new System.ApplicationException("Empty Data!").WithContent(response.Content);
+            return response.Data;
+        }
+    }
+}
